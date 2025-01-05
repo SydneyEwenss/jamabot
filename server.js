94d33a7018cd4ab2325fb6a2b41f661ccc580dd4
SydneyEwenss <sydneyewens06@gmail.com>
2025-01-05 01:57:11 +0000

    updated some styles

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./db'); // SQLite DB connection
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Serve static files (like CSS, JS, images) from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 // Route to display leaderboard
 app.get('/leaderboard', (req, res) => {
     // Sort by level and then by xp
-    db.all('SELECT username, level, xp, messagesSent, voiceTime FROM users ORDER BY level DESC, xp DESC LIMIT 10', (err, rows) => {
+    db.all('SELECT username, level, xp, messagesSent, voiceTime FROM users ORDER BY level DESC, xp DESC', (err, rows) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
